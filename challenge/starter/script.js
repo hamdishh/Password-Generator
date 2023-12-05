646bc205aabb7e9afce9b6d9a793d9185d0e735b
Hamdi <hamdishh2003@gmail.com>
2023-12-05 01:08:14 +0000

    Commit changes to repo

---
+++ challenge/starter/script.js
@@ -0,0 +1,147 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  var lengthOfPassword = prompt("Enter the length of the password. It must be between 8 and 128 characters:");
+
+  // Use logical operators to authenticate input for the lenght of the password
+  if (lengthOfPassword < 8 || lengthOfPassword > 128 || isNaN(lengthOfPassword)) {
+    console.log("Invalid password length entered by the user.");
+    alert("Password length is not valid. Please enter a number of characters between 8 and 128.");
+    return; 
+}
+
+
+var includeLowercase = confirm("Include lowercase characters?");
+var includeUppercase = confirm("Include uppercase characters?");
+var includeNumeric = confirm("Include numeric characters?");
+var includeSpecial = confirm("Include special characters?");
+
+// Ensure that at least one character type will be used by the user
+if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+  console.log("No character type selected by the user.");
+  alert("Please select at least one character type.");
+  return;
+}
+
+console.log("User input received successfully.");
+return {
+  length: passwordLength,
+  includeLowercase: includeLowercase,
+  includeUppercase: includeUppercase,
+  includeNumeric: includeNumeric,
+  includeSpecial: includeSpecial
+};
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
